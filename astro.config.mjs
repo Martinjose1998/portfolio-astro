470ac1050df3a235d1761df0bdedd7f83f930192
Martinjose1998 <65858159+Martinjose1998@users.noreply.github.com>
2025-03-03 14:05:44 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Martinjose1998.github.io',
+  base: 'portfolio-astro',
   integrations: [svelte()]
 });
